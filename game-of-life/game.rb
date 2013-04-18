c085bc9b34d65db81b843acd95b51a542f1c1765
P. Michael Holland <maikeru.horando@gmail.com>
2013-04-18 22:52:39 +0900

    Add two neighbouring cells case

--- game-of-life/game.rb
+++ game-of-life/game.rb
@@ -2,10 +2,18 @@
 
 require 'minitest/autorun'
 
+# 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+# 2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+# 3. Any live cell with two or three live neighbours lives on to the next generation.
+# 4. Any dead cell with exactly three live neighbours becomes a live cell.
+
 class Game
   def self.get_next_generation_from grid
-    if grid.include? '*'
+    live_cell_count = grid.count('*')
+    if live_cell_count == 1
       grid.sub! '*', '.'
+    elsif live_cell_count == 2
+      grid
     else
       grid
     end
@@ -44,6 +52,26 @@ ENDGRID
     expected_grid = <<-ENDGRID
 ........
 ........
+........
+........
+ENDGRID
+    result_grid = Game.get_next_generation_from input_grid
+    result_grid.must_equal expected_grid
+  end
+end
+
+describe "when given a grid with two neighbouring cells" do
+  it "should return the same grid" do
+    input_grid = <<-ENDGRID
+........
+...**...
+........
+........
+ENDGRID
+
+    expected_grid = <<-ENDGRID
+........
+...**...
 ........
 ........
 ENDGRID
