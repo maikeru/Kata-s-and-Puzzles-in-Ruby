d1ab6d94e8e9a600c22868573a2ba992163cf8cd
P. Michael Holland <maikeru.horando@gmail.com>
2013-04-18 23:45:44 +0900

    Implemented dead cell stays dead for zero and one neighbour

--- game-of-life/cell.rb
+++ game-of-life/cell.rb
@@ -1,12 +1,19 @@
 
 class Cell
-  attr_accessor :state
+  attr_accessor :state, :neighbours
+  def initialize
+    @neighbours = []
+  end
+
   def is_dead?
     @state == "dead"
   end
 
   def is_alive?
     @state == "alive"
+  end
+
+  def evaluate
   end
 end
 
--- game-of-life/tests/cell_tests.rb
+++ game-of-life/tests/cell_tests.rb
@@ -32,22 +32,25 @@ describe "when a cell is in an alive state" do
 end
 
 describe "when a cell is dead" do
-  #before do
-  #  @cell = Cell.new
-  #  @cell.state = "dead"
-  #end
 
-  #describe "and it has no neighbours" do
-  #  it "stays dead" do
-  #    @cell.neighbours.add(Cell.new)
-  #    @cell.neighbours.add(Cell.new)
-  #    @cell.evaluate
-  #    assert @cell.is_dead
-  #  end
-  #end
+  before do
+    @cell = Cell.new
+    @cell.state = "dead"
+  end
+
+  describe "and it has no neighbours" do
+    it "stays dead" do
+      @cell.evaluate
+      assert @cell.is_dead?
+    end
+  end
 
   describe "and it has one neighbour" do
-    it "stays dead"
+    it "stays dead" do
+      @cell.neighbours.push Cell.new
+      @cell.evaluate
+      assert @cell.is_dead?
+    end
   end
 
   describe "and it has two neighbours" do
