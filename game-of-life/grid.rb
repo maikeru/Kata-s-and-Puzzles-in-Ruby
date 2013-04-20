1b7aa60127569a70a278b9f7e9052e1eafc04004
P. Michael Holland <maikeru.horando@gmail.com>
2013-04-20 11:02:52 +0900

    Now adds cell directly above as a neighbour in the grid

--- game-of-life/grid.rb
+++ game-of-life/grid.rb
@@ -4,9 +4,31 @@ class Grid
   attr_reader :cells
   def initialize grid_string
     @cells = []
+    generate_from grid_string
+  end
+
+  private
+
+  def generate_from grid_string
     grid_lines = grid_string.split "\n"
     x = grid_lines[0].length
     y = grid_lines.size
-    (x * y).times { @cells.push Cell.new }
+    y.times do |y_pos|
+      x.times do |x_pos|
+        new_cell = Cell.new
+        previous_cell = @cells.last
+        @cells.push new_cell
+        if previous_cell
+          previous_cell.neighbours.push new_cell
+          new_cell.neighbours.push previous_cell
+        end
+        if y_pos > 0
+          above_cell_index = x_pos * (y_pos - 1)
+          above_cell = @cells[above_cell_index]
+          above_cell.neighbours.push new_cell
+          new_cell.neighbours.push above_cell
+        end
+      end
+    end
   end
 end
--- game-of-life/tests/grid_tests.rb
+++ game-of-life/tests/grid_tests.rb
@@ -1,15 +1,33 @@
 require 'minitest/autorun'
 require_relative '../grid'
 
-describe 'when a 4x4 grid string is passed to the grid' do
-  it 'generates a grid with 16 cells' do
-    grid_string = <<-ENDGRID
-....
-....
-....
-....
+eight_by_four_empty_grid = <<-ENDGRID
+........
+........
+........
+........
     ENDGRID
-    grid = Grid.new grid_string
-    grid.cells.count.must_equal 16
+describe 'when a 8x4 grid string is passed to the grid' do
+  grid_string = eight_by_four_empty_grid
+
+  let(:grid) { Grid.new grid_string }
+
+  it 'generates a grid with 16 cells' do
+    grid.cells.count.must_equal 32
   end
+
+  it 'puts the cell at (0,0) next to the one at (0,1)' do
+    cell0_0 = grid.cells[0]
+    cell0_1 = grid.cells[1]
+    cell0_0.neighbours.must_include cell0_1
+    cell0_1.neighbours.must_include cell0_0
+  end
+
+  it 'puts the cell at (0,0) next to the one at (1,0)' do
+    cell0_0 = grid.cells[0]
+    cell1_0 = grid.cells[8]
+    cell0_0.neighbours.must_include cell1_0
+    cell1_0.neighbours.must_include cell0_0
+  end
+  it 'puts the cell at (0,0) next to the one at (1,1)'
 end
